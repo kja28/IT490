5b663f6fb93b90dd21d4205d94b99e34b2e948ce
pkhatri13 <90870169+pkhatri13@users.noreply.github.com>
2023-03-21 12:27:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,20 @@ function login() {
     alert("Incorrect username or password.");
   }
 }
+
+function sendLoginDetails(username, password){
+    const xmlhttp = new XMLHttpRequest();
+  const url = "login.php?username="+username+"&password="+password;
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if(this.responseText.trim()!=""){
+          
+          let response = this.responseText;
+          alert(response);
+    
+        }
+      }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
