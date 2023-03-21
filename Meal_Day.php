c7d09c4964bb9161226d99af6ea7b33e649de752
Scottb34 <98284352+Scottb34@users.noreply.github.com>
2023-03-21 13:28:43 -0400

    Create Meal_Day.php

---
+++ Meal_Day.php
@@ -0,0 +1,27 @@
+<?php function getMeal($query){
+
+	$curl = curl_init();
+
+curl_setopt_array($curl, [
+	CURLOPT_URL => "https://random-recipes.p.rapidapi.com/ai-quotes/$query",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"X-RapidAPI-Host: random-recipes.p.rapidapi.com",
+		"X-RapidAPI-Key: f3e58460f7msh59bd4ab7d8245aap10982bjsn0b20d69d0da7"
+	],
+]);
+$response = curl_exec($curl);
+//$err = curl_error($curl);
+
+curl_close($curl);
+
+return $response['value'];
+
+
+}
