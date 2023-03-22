539ff05019d45671b6e6c154892573dbed8a8994
Scottb34 <98284352+Scottb34@users.noreply.github.com>
2023-03-22 12:01:46 -0400

    Create meals_tabel_update.sql

---
+++ meals_tabel_update.sql
@@ -0,0 +1,4 @@
+alter table meals modify title varchar(255) not null;
+alter table meals modify ingredients varchar(255) not null;
+alter table meals modify instructions varchar(255) not null;
+alter table meals modify image varchar(255) not null;
