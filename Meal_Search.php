6f8b3723bf783bbe9d403a468325d35449ea7c63
Scottb34 <98284352+Scottb34@users.noreply.github.com>
2023-03-24 11:47:29 -0400

    Create Meal_Search.php

---
+++ Meal_Search.php
@@ -0,0 +1,39 @@
+<?php function search($queryx){
+
+
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+	CURLOPT_URL => "https://yummly2.p.rapidapi.com/feeds/auto-complete?q=chicken%20soup",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"X-RapidAPI-Host: yummly2.p.rapidapi.com",
+		"X-RapidAPI-Key: f3e58460f7msh59bd4ab7d8245aap10982bjsn0b20d69d0da7"
+	],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
+}
+$response = curl_exec($curl);
+//$err = curl_error($curl);
+
+curl_close($curl);
+
+return $response['value'];
+}
