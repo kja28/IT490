9333429299aeadaceb07679cea3c6c5ae0c8bd92
pkhatri13 <90870169+pkhatri13@users.noreply.github.com>
2023-05-07 21:03:09 -0400

    Update verify.php

--- verify.php
+++ verify.php
@@ -12,7 +12,7 @@ if(!isset($code){
 if(isset($_POST['code'])){
     // Check if verification code matches
     $code = $_POST['code'];
-    if ($code == $_SESSION['code']) {
+    if ($code == $_SESSION['verification_code']) {
         // Verification successful, set email session variable
         $_SESSION['authenticated'] = true;
 
